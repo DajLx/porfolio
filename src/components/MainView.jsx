d35224503070ce62385db5428bec278b11ec8538
alex <durandaj027@gmail.com>
2023-06-30 15:53:17 -0400

    sixth commit

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -17,25 +17,31 @@ const MainView = () => {
           : "mainViewContainer"
       }
     >
-      ¡Hola! ¡Bienvenido/a a mi perfil! Aquí podrás conocer más sobre mí y mis
-      proyectos. No te preocupes, todo es gratis y sin compromiso. En la sección
-      "Acerca de mí" encontrarás información sobre mi personalidad, actitud y
-      <img
-        src={img2}
-        className={active ? "imgSize-2 imgSize-2-active" : "imgSize-2"}
-        style={{
-          float: "right",
-          margin: "0rem",
-        }}
-      />{" "}
-      experiencia laboral. En "Portafolio" podrás ver mis proyectos y un juego
-      dinámico de colores que te encantará. Si deseas contactarme, ve a la
-      sección "Contacto", donde encontrarás mis datos personales y medios para
-      comunicarte conmigo de forma fácil y sencilla. Recuerda que siempre hay
-      solución a los problemas, solo necesitas constancia, perseverancia y
-      paciencia para lograr grandes cosas. Y si te cansas del tema, ¡puedes
-      cambiarlo con solo un clic en el botón a tu derecha! ¡Gracias por visitar
-      mi perfil!
+      <div className="text-mainView">
+        {" "}
+        <p>
+          ¡Hola! ¡Bienvenido/a a mi perfil! Aquí podrás conocer más sobre mí y
+          mis proyectos. No te preocupes, todo es gratis y sin compromiso. En la
+          sección "Acerca de mí" encontrarás información sobre mi personalidad,
+          actitud y
+          <img
+            src={img2}
+            className={active ? "imgSize-2 imgSize-2-active" : "imgSize-2"}
+            style={{
+              float: "right",
+              margin: "0rem",
+            }}
+          />{" "}
+          experiencia laboral. En "Portafolio" podrás ver mis proyectos y un
+          juego dinámico de colores que te encantará. Si deseas contactarme, ve
+          a la sección "Contacto", donde encontrarás mis datos personales y
+          medios para comunicarte conmigo de forma fácil y sencilla. Recuerda
+          que siempre hay solución a los problemas, solo necesitas constancia,
+          perseverancia y paciencia para lograr grandes cosas. Y si te cansas
+          del tema, ¡puedes cambiarlo con solo un clic en el botón a tu derecha!
+          ¡Gracias por visitar mi perfil!
+        </p>
+      </div>
     </div>
   );
 };
